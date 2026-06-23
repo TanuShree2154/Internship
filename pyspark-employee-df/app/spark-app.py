e3a134d8df31b0716a5a5dcda56c2c62251e8dfc
TanuShree2154 <tanushreesoni6644@gmail.com>
2026-06-23 01:15:45 -0700

    Add files via upload

--- pyspark-employee-df/app/spark-app.py
+++ pyspark-employee-df/app/spark-app.py
@@ -1,29 +1,24 @@
 from pyspark.sql import SparkSession
-spark = SparkSession.builder.appName("EmployeeRDD").getOrCreate()
-sc= spark.sparkContext
+from pyspark.sql.functions import col
 
-rdd = sc.textFile("employee.csv")
-#sort all employees by salary in descending order and display the results on the console
-header = rdd.first()
+spark = SparkSession.builder.appName("SalesAnalysis").getOrCreate()
 
-rdd_structured = rdd.filter(lambda x: x.strip() != header.strip()) \
-    .filter(lambda x: len(x.split(",")) == 4) \
-    .map(lambda x: x.split(",")) \
-    .map(lambda x: (int(x[0]), x[1], x[2], int(x[3])))
 
-sorted_employees = rdd_structured.sortBy(lambda x: x[3], ascending=False)
+df = spark.read.option("header", True).csv("/workspace/employee.csv", inferSchema=True)
+# 1. Sort products by sales (descending)
+sorted_df = df.orderBy(col("sales").desc())
+print("Sorted Products by Sales ")
+sorted_df.show()
 
-print("Employees sorted by salary ")
-print(sorted_employees.collect())
+# 2. Top 3 products
+print("Top 3 Products ")
+df.orderBy(col("sales").desc()).show(3)
 
-#calculate the total salary paid in each department and print the department-wise totals
-dept_salary_rdd = rdd_structured.map(lambda x: (x[2], x[3]))
-dept_total_salary = dept_salary_rdd.reduceByKey(lambda a, b: a + b)
-print(dept_total_salary.collect())
-#identify the top three highest-paid employees and save the output to a file
-sorted_rdd = rdd_structured.sortBy(lambda x: x[3], ascending=False)
-top_3 = sorted_rdd.take(3)
-top_3_rdd = sc.parallelize(top_3)
-print(">>> Reached output write step")
-top_3_rdd.saveAsTextFile("/workspace/output/top_3_employees")
-print(">>> Write completed")
+# 3. Filter sales > 80000
+filtered_df = df.filter(col("sales") > 80000)
+
+
+filtered_df.write.mode("overwrite").csv("/workspace/output/filtered_sales", header=True)
+print("Filtered data saved successfully!")
+
+spark.stop()
